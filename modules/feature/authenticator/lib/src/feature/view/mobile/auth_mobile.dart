ca22c47245065b87a6994eb174d313bc53d0c3a5
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2023-12-15 00:18:04 -0300

    feat: auth mobile page

--- modules/feature/authenticator/lib/src/feature/view/mobile/auth_mobile.dart
+++ modules/feature/authenticator/lib/src/feature/view/mobile/auth_mobile.dart
@@ -11,42 +11,139 @@ class AuthMobile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            surfaceTintColor: Colors.transparent,
-            pinned: true,
-            elevation: 0,
-            leadingWidth: 100,
-            leading: CupertinoButton(
-              padding: const EdgeInsets.only(left: 8),
-              onPressed: () => Modular.to.pop(),
-              child: Row(
-                children: [
-                  Icon(
-                    CupertinoIcons.arrow_left,
-                    color: SystemMode.isDark(context)
-                        ? Colors.white
-                        : Colors.black,
-                  ),
-                  const SizedBox(width: 4),
-                  Text(
-                    'Voltar',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyLarge! //
-                        .copyWith(
-                          fontWeight: FontWeight.w600,
+      body: Column(
+        children: [
+          Expanded(
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  surfaceTintColor: Colors.transparent,
+                  pinned: true,
+                  elevation: 0,
+                  leadingWidth: 100,
+                  leading: CupertinoButton(
+                    padding: const EdgeInsets.only(left: 8),
+                    onPressed: () => Modular.to.pop(),
+                    child: Row(
+                      children: [
+                        Icon(
+                          CupertinoIcons.arrow_left,
+                          color: SystemMode.isDark(context)
+                              ? Colors.white
+                              : Colors.black,
                         ),
+                        const SizedBox(width: 4),
+                        Text(
+                          'Voltar',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyLarge! //
+                              .copyWith(
+                                fontWeight: FontWeight.w600,
+                              ),
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
+                ),
+                SliverToBoxAdapter(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 200,
+                    child: const Image(
+                      image: backgroundChurch,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SliverPadding(padding: EdgeInsets.symmetric(vertical: 8)),
+                SliverToBoxAdapter(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Vamos fazer o login', // TODO: move to i18n
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge! //
+                              .copyWith(
+                                fontWeight: FontWeight.bold,
+                              ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          'Tendo sua conta, abaixo você pode fazer o login dentro do acampamento da paroquia', // TODO: move to i18n
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelMedium! //
+                              .copyWith(
+                                  // fontWeight: FontWeight.bold,
+                                  ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SliverPadding(padding: EdgeInsets.symmetric(vertical: 8)),
+                SliverToBoxAdapter(
+                  child: CustomTextFormField(
+                    // focusNote: viewStore.value.onFocus,
+                    // controller: viewStore.value.textEditingController,
+                    boxDecorationColor: SystemMode.isDark(context)
+                        ? Colors.black
+                        : Colors.grey.shade200,
+                    labelText: 'Preencha seu nickname', // TODO: Move to i18n
+                  ),
+                ),
+                const SliverPadding(padding: EdgeInsets.symmetric(vertical: 8)),
+                SliverToBoxAdapter(
+                  child: CustomTextFormField(
+                    // focusNote: viewStore.value.onFocus,
+                    // controller: viewStore.value.textEditingController,
+                    boxDecorationColor: SystemMode.isDark(context)
+                        ? Colors.black
+                        : Colors.grey.shade200,
+                    labelText: 'Preencha sua senha', // TODO: Move to i18n
+                  ),
+                ),
+              ],
             ),
           ),
-          const SliverToBoxAdapter(
-            child: SizedBox(
-              height: 100,
-              child: Text('Auth'),
+          AnimatedOpacity(
+            opacity: View.of(context).viewInsets.bottom > 0.0 ? 0 : 1,
+            duration: Durations.medium1,
+            child: const Divider(
+              thickness: 0.2,
+              height: 1,
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.only(
+              top: 8,
+              bottom: MediaQuery.of(context).padding.bottom,
+            ),
+            child: AnimatedButton(
+              isFocus: View.of(context).viewInsets.bottom > 0.0,
+              // isDisabled: value.isLoading,
+              onPress: () {},
+              enableColor: Colors.deepPurple.shade300,
+              disableColor: SystemMode.isDark(context)
+                  ? Colors.deepPurple.shade500
+                  : Colors.deepPurple.shade100,
+              disabledChild: const CircularProgressIndicator.adaptive(),
+              child: Text(
+                'Proximo',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium! //
+                    .copyWith(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+              ),
             ),
           ),
         ],
