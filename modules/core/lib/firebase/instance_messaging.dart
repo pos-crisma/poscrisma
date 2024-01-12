69c8b9141c079f21c3b46d7ca9b10f162012df6e
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2024-01-12 12:54:31 -0300

    feat: remove fcm to web browser

--- modules/core/lib/firebase/instance_messaging.dart
+++ modules/core/lib/firebase/instance_messaging.dart
@@ -5,40 +5,33 @@ final messaging = FirebaseMessaging.instance;
 
 class MessagingInstance {
   Future<void> initNotification() async {
-    await messaging.requestPermission();
-    await messaging.setAutoInitEnabled(true);
+    if (!kIsWeb) {
+      await messaging.requestPermission();
+      await messaging.setAutoInitEnabled(true);
 
-    final fcmToken = await messaging.getToken();
-    final apnsToken = await messaging.getAPNSToken();
+      final fcmToken = await messaging.getToken();
+      final apnsToken = await messaging.getAPNSToken();
 
-    if (kDebugMode) {
-      print('Firebase Messaging Token: $fcmToken');
-      print('APNs Token: $apnsToken');
-    }
-
-    messaging.getInitialMessage().then((value) {
       if (kDebugMode) {
-        print('messaging.getInitialMessage(): $value');
+        print('Firebase Messaging Token: $fcmToken');
+        print('APNs Token: $apnsToken');
       }
-    });
 
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Got a message whilst in the foreground!');
-      print('Message data: ${message.data}');
+      messaging.getInitialMessage().then((value) {
+        if (kDebugMode) {
+          print('messaging.getInitialMessage(): $value');
+        }
+      });
 
-      if (message.notification != null) {
-        print('Message also contained a notification: ${message.notification}');
-      }
-    });
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        print('Got a message whilst in the foreground!');
+        print('Message data: ${message.data}');
 
-    // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+        if (message.notification != null) {
+          print(
+              'Message also contained a notification: ${message.notification}');
+        }
+      });
+    }
   }
-
-  // Future<void> _firebaseMessagingBackgroundHandler(
-  //     RemoteMessage message) async {
-  //   // If you're going to use other Firebase services in the background, such as Firestore,
-  //   // make sure you call `initializeApp` before using other Firebase services.
-
-  //   print("Handling a background message: ${message.messageId}");
-  // }
 }
