0539b3073c350bef93bd3c4b10af1da3be2cb227
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2024-02-04 00:00:08 -0300

    feat: result type

---
+++ modules/feature/private/schedule/lib/src/feature/result/view/result_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ResultPage extends StatelessWidget {
+  const ResultPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
