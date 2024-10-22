225ddcaba5905a6586f33064ee86813096688609
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2024-10-22 16:02:12 -0300

    remove importing not used

--- lib/module/shared/network/http_provider.dart
+++ lib/module/shared/network/http_provider.dart
@@ -5,7 +5,6 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
-import 'package:get_it/get_it.dart';
 import 'package:poscrisma/index.dart';
 import 'package:http/http.dart' as http;
 
